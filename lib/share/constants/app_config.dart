7e753916e2e008927667eedc8cd24acf1c8410bd
maichu <chuthimai18245@gmail.com>
2026-01-01 15:17:35 +0700

    Sửa pod để chạy đc trên ios

--- lib/share/constants/app_config.dart
+++ lib/share/constants/app_config.dart
@@ -1,6 +1,7 @@
 class AppConfig {
   static const baseUrl = String.fromEnvironment(
       'BASE_URL',
-      defaultValue: 'http://192.168.0.103:3333'
+      // defaultValue: 'http://192.168.0.103:3333',
+      defaultValue: 'https://3.25.246.68/hie'
   );
 }
